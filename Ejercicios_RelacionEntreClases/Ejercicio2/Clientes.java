2f4adf3d5ea641d29c0f3ec7f7292051de2f4b39
darwme <darw.mendz@gmail.com>
2022-10-09 01:33:35 -0500

    Nueva actualización

---
+++ Ejercicios_RelacionEntreClases/Ejercicio2/Clientes.java
@@ -0,0 +1,25 @@
+package Ejercicio2;
+import java.util.GregorianCalendar;
+public class Clientes {
+    String nombre, dni;
+    GregorianCalendar fechaNacimiento;
+    public Clientes(String nom, String doc_i, GregorianCalendar fn){
+        nombre=nom;
+        dni=doc_i;
+        fechaNacimiento=fn;
+    }
+    public String getNombre(){
+        return nombre;
+    }
+    public String getDni(){
+        return dni;
+    }
+    public GregorianCalendar getFechaNacimiento(){
+        return fechaNacimiento;
+    }
+
+    @Override
+    public String toString(){
+        return this.getNombre() + "\t" + this.getDni() + "\t" + this.getFechaNacimiento().getTime();
+    }
+}
